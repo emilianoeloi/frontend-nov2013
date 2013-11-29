2f1a00d1ed964f1cf1ef17c7248f79e2a0da4ef8
Fernando Augusto Rocha <fernandonanet_676@hotmail.com>
2013-11-28 22:05:03 -0200

    Commites Iniciais
    Exercicios Feitos em sala, que foi pedido para fazer o commit.

---
+++ alunos/fernandoarocha/Front-END/Aula1/assets/js/main.js
@@ -0,0 +1,9 @@
+function display_alert()
+    {
+        alert("Gravado com sucesso.");
+    }
+
+function minhaFuncao(parametro)
+    {
+        window.console.log('meu log', parametro);
+    }
---
+++ alunos/fernandoarocha/Front-END/Aula1/assets/js/tabela.js
@@ -0,0 +1,9 @@
+function elemento(e1)
+{
+    return document.getElementById(e1);
+
+}
+
+function soma(elemento1, elemento2){
+    return elemento1+elemento2;
+}
