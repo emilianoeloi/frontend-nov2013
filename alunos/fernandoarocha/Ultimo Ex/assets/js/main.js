7caa93a6959191f145ad70106b88bf874e02db46
Fernando Augusto Rocha <fernandonanet_676@hotmail.com>
2013-12-14 14:34:15 -0200

    Conserto

--- alunos/fernandoarocha/Ultimo Ex/assets/js/main.js
+++ alunos/fernandoarocha/Ultimo Ex/assets/js/main.js
@@ -13,39 +13,39 @@ tela.setAttribute('id', 'tela')
 var html = document.createElement('a');
 html.setAttribute('href', '//www.google.com');
 html.setAttribute('name', 'Novas Tags');
-html.innerText = "Novas tags e recursos para produzir páginas WEB";
+html.innerHTML = 'Novas tags e recursos para produzir páginas WEB</br>';
 var css = document.createElement('a');
 css.setAttribute('href', '//www.google.com');
 css.setAttribute('name', 'Novas Tags');
-css.innerText = "Diminuindo o trabalho do javascript com Layout";
+css.innerHTML = 'Diminuindo o trabalho do javascript com Layout</br>';
 var js = document.createElement('a');
 js.setAttribute('href', '//www.google.com');
 js.setAttribute('name', 'Novas Tags');
-js.innerText = "Linguagem fundamental e soberana nos navegadores WEB";
+js.innerHTML = 'Linguagem fundamental e soberana nos navegadores WEB</br>';
 var recorte = document.createElement('a');
 recorte.setAttribute('href', '//www.google.com');
 recorte.setAttribute('name', 'Novas Tags');
-recorte.innerText = "Photoshop o companheiro Inseparável";
+recorte.innerHTML = '<img src="./assets/img/iconeEstrela.png" style="width: 15"></img>Photoshop o companheiro Inseparável</br>';
 
 var recortet = document.createElement('label');
 recortet.setAttribute('name', 'Recorte');
 recortet.setAttribute('class', 'ex1');
-recortet.innerText = "Recorte";
+recortet.innerHTML = 'Recorte';
 
 var htmlt = document.createElement('label');
 htmlt.setAttribute('name', 'html');
 htmlt.setAttribute('class', 'ex1');
-htmlt.innerText = "HTML5 [Conteudo]";
+htmlt.innerHTML = 'HTML5 <span class="verde">[Conteudo]</span>';
 
 var csst = document.createElement('label');
 csst.setAttribute('name', 'css');
 csst.setAttribute('class', 'ex1');
-csst.innerText = "CSS [ESTILO]";
+csst.innerHTML = 'CSS <span class="vermelho">[Estilo]</span>';
 
 var jst = document.createElement('label');
 jst.setAttribute('name', 'js');
 jst.setAttribute('class', 'ex1');
-jst.innerText = "Java Script Programação";
+jst.innerHTML = 'Java Script <span class="verde">[Programação]</span>';
 
 tela.appendChild(recortet);
 tela.appendChild(br6);
@@ -62,4 +62,5 @@ tela.appendChild(br5);
 tela.appendChild(jst);
 tela.appendChild(br1);
 tela.appendChild(js);
-document.body.appendChild(tela);
+document.getElementById('container').appendChild(tela);
+//document.body.appendChild(tela);
